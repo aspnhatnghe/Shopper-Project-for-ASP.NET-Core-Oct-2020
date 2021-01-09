4138eb33e36ab58d2019cedd2a88cddf037aba97
NN <hyhien@gmail.com>
2021-01-09 14:54:00 +0700

    Add Loai Entity

---
+++ ShopperProject/Data/Loai.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ShopperProject.Data
+{
+    [Table("Loai")]
+    public class Loai
+    {
+        [Key]
+        public int MaLoai { get; set; }
+        public string TenLoai { get; set; }
+        public int? MaLoaiCha { get; set; }
+        [ForeignKey("MaLoaiCha")]
+        public Loai LoaiCha { get; set; }
+    }
+}
