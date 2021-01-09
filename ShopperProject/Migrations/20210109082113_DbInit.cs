7b1ea157b2af1bd6e3393ddae0092dbee81d952f
NN <hyhien@gmail.com>
2021-01-09 15:24:34 +0700

    Generate Db

---
+++ ShopperProject/Migrations/20210109082113_DbInit.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace ShopperProject.Migrations
+{
+    public partial class DbInit : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Loai",
+                columns: table => new
+                {
+                    MaLoai = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TenLoai = table.Column<string>(nullable: true),
+                    MaLoaiCha = table.Column<int>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Loai", x => x.MaLoai);
+                    table.ForeignKey(
+                        name: "FK_Loai_Loai_MaLoaiCha",
+                        column: x => x.MaLoaiCha,
+                        principalTable: "Loai",
+                        principalColumn: "MaLoai",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "HangHoa",
+                columns: table => new
+                {
+                    MaHh = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    TenHh = table.Column<string>(maxLength: 150, nullable: true),
+                    Hinh = table.Column<string>(maxLength: 150, nullable: true),
+                    DonGia = table.Column<double>(nullable: false),
+                    SoLuongTon = table.Column<int>(nullable: false),
+                    GiamGia = table.Column<byte>(nullable: true),
+                    MoTa = table.Column<string>(nullable: true),
+                    MaLoai = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_HangHoa", x => x.MaHh);
+                    table.ForeignKey(
+                        name: "FK_HangHoa_Loai_MaLoai",
+                        column: x => x.MaLoai,
+                        principalTable: "Loai",
+                        principalColumn: "MaLoai",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_HangHoa_MaLoai",
+                table: "HangHoa",
+                column: "MaLoai");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Loai_MaLoaiCha",
+                table: "Loai",
+                column: "MaLoaiCha");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "HangHoa");
+
+            migrationBuilder.DropTable(
+                name: "Loai");
+        }
+    }
+}
--- ShopperProject/Startup.cs
+++ ShopperProject/Startup.cs
@@ -5,9 +5,11 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using ShopperProject.Data;
 
 namespace ShopperProject
 {
@@ -24,6 +26,9 @@ namespace ShopperProject
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddDbContext<ShopperDbContext>(option => {
+                option.UseSqlServer(Configuration.GetConnectionString("Shopper"));
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
