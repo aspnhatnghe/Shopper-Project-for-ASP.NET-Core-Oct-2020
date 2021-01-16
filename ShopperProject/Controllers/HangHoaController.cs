2c818c756c67ad85ec67477b9204277b982bcd9d
NN <aspcorenn@gmail.com>
2021-01-16 14:22:09 +0700

    get hangHoas

--- ShopperProject/Controllers/HangHoaController.cs
+++ ShopperProject/Controllers/HangHoaController.cs
@@ -1,20 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
 using ShopperProject.Data;
+using ShopperProject.Models;
+using System.Linq;
 
 namespace ShopperProject.Controllers
 {
     public class HangHoaController : Controller
     {
         private readonly ShopperDbContext _context;
+        private readonly IMapper _mapper;
 
-        public HangHoaController(ShopperDbContext context)
+        public HangHoaController(ShopperDbContext context, IMapper mapper)
         {
             _context = context;
+            _mapper = mapper;
         }
 
         public IActionResult Index()
         {
-            return View();
+            var data = _context.HangHoas.ToList();
+
+            var dsHangHoa = _mapper.Map<HangHoaViewModel>(data);
+
+            return View(dsHangHoa);
         }
     }
 }
